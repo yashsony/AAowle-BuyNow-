30948695e0f1c911abafbd99254646d94c9359eb
yashsoni <yash.soni@razorpay.com>
2022-05-29 16:18:17 +0530

    button working

---
+++ theme-app-extension/assets/app.js
@@ -0,0 +1,49 @@
+
+let APIurl =   `${window.location.pathname}.js`;
+var productDetails = {};
+var currPrice = 0;
+
+fetch(APIurl)           
+    .then(response => response.json())
+    .then(data => {
+            data.variants.forEach((item) => {
+                productDetails[item.id] = (item.price/100);
+            });
+            console.log(data, productDetails);
+            var productForm = formSerialize(document.querySelector('[action="/cart/add"]'));
+            var formValue = JSON.parse(productForm);
+            currPrice = productDetails[formValue.id];
+            document.getElementById("price").innerHTML = currPrice;
+            console.log(currPrice, "current Price");
+
+
+
+            document.querySelector('[action="/cart/add"]').addEventListener('change', ()=>{
+                var productForm = formSerialize(document.querySelector('[action="/cart/add"]'));
+                var formValue = JSON.parse(productForm, "current Price");
+                currPrice = productDetails[formValue.id];
+                document.getElementById("price").innerHTML = currPrice;
+                console.log(currPrice, "current Price");
+            });
+    });
+
+
+
+
+
+
+
+function formSerialize(myDocument){
+    var obj = {};
+    var elements = myDocument.querySelectorAll( "input, select, textarea" );
+    for( var i = 0; i < elements.length; ++i ) {
+        var element = elements[i];
+        var name = element.name;
+        var value = element.value;
+  
+        if( name ) {
+            obj[ name ] = value;
+        }
+    }
+    return JSON.stringify( obj );
+}
