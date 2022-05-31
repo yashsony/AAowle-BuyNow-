27c84fc5287c7fe4a8ed41ab96d3c168e8df49f0
yashsoni <yash.soni@razorpay.com>
2022-05-31 10:47:39 +0530

    formatting

--- theme-app-extension/assets/app.js
+++ theme-app-extension/assets/app.js
@@ -1,42 +1,50 @@
 
 let APIurl =   `${window.location.pathname}.js`;
-var productDetails = {};
+var productDetails = {}; // stores variantId => Varaintprice
 var currPrice = 0;
 
 fetch(APIurl)           
     .then(response => response.json())
     .then(data => {
             data.variants.forEach((item) => {
-                productDetails[item.id] = (item.price/100);
+                productDetails[item.id] = (item.price/100); // get Details of product varaint from the API
             });
             console.log(data, productDetails);
             var productForm = formSerialize(document.querySelector('[action="/cart/add"]'));
-            var formValue = JSON.parse(productForm);
-            currPrice = productDetails[formValue.id];
+            var formValue = JSON.parse(productForm); // find the selected variantID on the store
+            currPrice = productDetails[formValue.id]; // find price of the selected variantID
             document.getElementById("price").innerHTML = currPrice;
             document.getElementById("price-btn").href = createCheckoutUrl(formValue.id);
-            console.log(currPrice, "current Price");
 
 
-
-            document.querySelector('[action="/cart/add"]').addEventListener('change', ()=>{
+            // add onchange event listner on form element, So that when user changes product variant then we should get new Price accordingly
+            document.querySelector('[action="/cart/add"]').addEventListener('change', () => { 
                 var productForm = formSerialize(document.querySelector('[action="/cart/add"]'));
                 var formValue = JSON.parse(productForm, "current Price");
                 currPrice = productDetails[formValue.id];
                 document.getElementById("price").innerHTML = currPrice;
                 document.getElementById("price-btn").href = createCheckoutUrl(formValue.id);
-                console.log(currPrice, "current Price");
             });
-    });
+    }).catch(() => {
+        document.getElementById("price").innerHTML = "{Price}";
+    })
 
 
-
+/**
+ * create checkout URl from the VariantID
+ * @param {id of the selected variant} variantId 
+ * @returns 
+ */
 function createCheckoutUrl(variantId) {
     return `/cart/${variantId}:1`;
 }
 
 
-
+/**
+ * serialize form element into data
+ * @param {HTMLDomElement} myDocument 
+ * @returns 
+ */
 function formSerialize(myDocument){
     var obj = {};
     var elements = myDocument.querySelectorAll( "input, select, textarea" );
